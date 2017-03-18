26298305f488f23e38311cec9f6dc40452c2e8aa
Mohamed Saleh Mostafa <msaleh@gistic.org>
2017-03-18 16:07:23 +0300

    [ADD] get partner rate
    Using each partner element in partners page, get the partner id. Use it
    to query the partner page.

--- get_kfit.rb
+++ get_kfit.rb
@@ -1,3 +1,5 @@
+# Assumption:If a partner not rated yet, will be give -1.
+
 require 'net/http'
 require 'nokogiri' 
 
@@ -7,13 +9,31 @@ PARTNERS_URL = 'https://access.kfit.com/partners/'
 def get_page(page_url)
 	response = Net::HTTP.get_response(URI(page_url))
 	if response.is_a?(Net::HTTPSuccess)
-		Nokogiri::HTML(response.body)
+		return Nokogiri::HTML(response.body)
 	else
 		puts "Error, Can't fetch this page#{page_url}"
 	end
 end
 
+# Return a partner id from its element.
+def get_partner_id(partner_element)
+	# partner_element contains all info, we need just the id in third row
+	rows = partner_element.text.split("\n")
+	return rows[2].scan(/\d+/)[0].to_i
+end
+
+def get_partner_rate(partner_page)
+	rate_node = partner_page.css('.rating').first
+	# If a partner not rated yet, return -1
+	return rate_node ? rate_node.text.to_f : -1
+end
+
+def get_partner(partner_id)	
+	page = get_page(PARTNERS_URL+partner_id.to_s)	
+	rate = get_partner_rate(page)
+	puts rate
+end
 
 partners_page = get_page(PARTNERS_URL)
 partners_elements = partners_page.xpath('//script[contains(text(),"kfitMap.outlets.push")]')
-puts partners_elements.length
+partners_elements.each{|p| get_partner(get_partner_id(p))}
